7b90075a05d6ed1eaec63e4b55e61123ac19687b
lory9894 <lory9894@gmail.com>
2022-06-11 17:34:15 +0200

    iniziato la relazione e visualizzato un po' di dati

---
+++ all_coordinates.py
@@ -0,0 +1,10 @@
+#questo file è stato fatto solo per visualizzare su una mappa tutte le coordinate
+import pandas as pd
+
+
+path = "./Brightkite_totalCheckins.txt"
+df = pd.read_table(path, delimiter='\t', names=['source', 'date', 'lat', 'lon', 'hash'])
+print(df.head()[['lon', 'lat']])
+with open('all_coordinates.txt', 'w') as f:
+    for index, row in df.iterrows():
+        f.write(f"{row['lon']}\t{row['lat']}\n")
